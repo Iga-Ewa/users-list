567bf308aa25e857664baa01442aae7ef5be34df
Iga-Ewa <alberskaiga@gmail.com>
2019-11-05 16:46:07 +0100

    Updates

---
+++ precache-manifest.e63632fced138cb92f68d343eefcaa9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288f3e495d186093c75eee604c763ae9",
+    "url": "/users-list/index.html"
+  },
+  {
+    "revision": "faeb038a4bc4052ca9a4",
+    "url": "/users-list/static/css/main.087f004e.chunk.css"
+  },
+  {
+    "revision": "f987ba54d7f8a7512792",
+    "url": "/users-list/static/js/2.6edbfc48.chunk.js"
+  },
+  {
+    "revision": "faeb038a4bc4052ca9a4",
+    "url": "/users-list/static/js/main.a1377116.chunk.js"
+  },
+  {
+    "revision": "0abfc31f3ab0752b8f4a",
+    "url": "/users-list/static/js/runtime-main.a3f67f48.js"
+  }
+]);
